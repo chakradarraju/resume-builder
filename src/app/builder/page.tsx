874cc4ec06cea3bcb4725d932b7f101d23d04313
Chakradar Raju <chakradarraju@gmail.com>
2024-12-05 23:20:08 +0530

    Add fullstory

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -5,23 +5,31 @@ import { useEffect } from "react";
 import { useProfile } from '../ProfileContext';
 import { useConfig } from "../ConfigContext";
 import Profile from "@/types/profile";
-import mixpanel from "mixpanel-browser";
+//import mixpanel from "mixpanel-browser";
+import { FullStory, init } from '@fullstory/browser';
+
+const FULLSTORY_ORG_ID = 'o-228D0K-na1';
 
 const Page: React.FC = () => {
   const {profile, setProfile} = useProfile();
   const {printMode} = useConfig();
 
   useEffect(() => {
-    mixpanel.init("f7fb8a723ec41c96963a52173e0bef49", {
-      debug: true,
-      track_pageview: true,
-      ignore_dnt: true,
-      record_sessions_percent: 100,
-      persistence: "localStorage",
-      api_host: 'https://api-in.mixpanel.com',
-    });
-    mixpanel.start_session_recording();
+    if (typeof window !== 'undefined') {
+      init({ orgId: FULLSTORY_ORG_ID });
+    }
   }, []);
+  // useEffect(() => {
+  //   mixpanel.init("f7fb8a723ec41c96963a52173e0bef49", {
+  //     debug: true,
+  //     track_pageview: true,
+  //     ignore_dnt: true,
+  //     record_sessions_percent: 100,
+  //     persistence: "localStorage",
+  //     api_host: 'https://api-in.mixpanel.com',
+  //   });
+  //   mixpanel.start_session_recording();
+  // }, []);
 
   useEffect(() => {
     if (typeof window !== 'undefined' && profile === null) {
